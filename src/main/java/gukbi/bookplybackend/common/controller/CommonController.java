7d47d632ddf2ef591f9d725fc2928cebfe049ede
dlsghths <dlsghths@gmail.com>
2024-04-22 01:13:23 +0900

    메인화면 추천도서 정보 받아오기 기능 추가

--- src/main/java/gukbi/bookplybackend/common/controller/CommonController.java
+++ src/main/java/gukbi/bookplybackend/common/controller/CommonController.java
@@ -3,35 +3,33 @@ package gukbi.bookplybackend.common.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gukbi.bookplybackend.common.dto.ResponseDTO;
 import gukbi.bookplybackend.common.service.CommonService;
 
 @RestController
-@RequestMapping("/sidebar")
 public class CommonController { // 공통으로 처리가 필요한 기능들 추가
 
   @Autowired
   CommonService commonService;
 
   // 대메뉴 가져오기
-  @GetMapping("/largeMenu/{level}")
+  @GetMapping("/sidebar/largeMenu/{level}")
   public ResponseDTO getLargeMenu(@PathVariable(value = "level") int level) {
     ResponseDTO res = commonService.getLargeMenu(level);
     return res;
   }
 
   // 소메뉴 가져오기
-  @GetMapping("/smallMenu")
+  @GetMapping("/sidebar/smallMenu")
   public ResponseDTO getSmallMenu() {
     ResponseDTO res = commonService.getSmallMenu();
     return res;
   }
 
   // 추천도서 등록정보 가져오기
-  @GetMapping(value = "/sugBookInfo")
+  @GetMapping(value = "/main/sugBookInfo")
   public ResponseDTO sugBookInfo() {
     ResponseDTO res = commonService.sugBookInfo();
     return res;
