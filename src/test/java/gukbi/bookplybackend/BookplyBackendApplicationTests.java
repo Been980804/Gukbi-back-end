17c88fdfe40dfc7b5c2b672a03909fca907d8929
dlsghths <dlsghths@gmail.com>
2024-05-31 11:53:43 +0900

    카카오 client id 값 추가

--- src/test/java/gukbi/bookplybackend/BookplyBackendApplicationTests.java
+++ src/test/java/gukbi/bookplybackend/BookplyBackendApplicationTests.java
@@ -18,9 +18,26 @@ class BookplyBackendApplicationTests {
 		System.out.println(jasyptEncoding("/JCzYoX1INM/SY99a26cXb7BaQKMf8Mz57Mmr0EW1/wtL8mY9KrksHH9Z2A0W4rCVF0+lzw3UlpX9N8jgR9Sbg=="));
 		System.out.println(jasyptEncoding("F+4VhBdLjcDofFcxUKE5FNe7XK0kyymeNwplUNj0DpbshxCcuLnmKgvgc7DaUoiu/cJpbbJC5naDvlKeS8bGjTAjXSlDJnX//iAcWeXpNGqwZxD96ERl/3cdzrN603pLhtYGOETnVBOO0BAu9FHz/A=="));
 		System.out.println(jasyptEncoding("FOpmMhy8IQiCu57gqx9P14bsIFCn5nvyK8BYLL+3ctgUx7sXXnNQXmxlrqsw5m8bXJSUm7/79PlDEJO0fI0nG+NsZhos89dquaUfpm0ap8Q="));
+
+		System.out.println(jasyptEncoding("Qx+5rqCqYS91wi7blQjfGnWRm1ZE6K/xsoPRUa3na9p6Ee1HiFcdlojah4GqbPLCj3dSMiDdLa4VOJA3JV/ESZHt2Yrml5+IdSPdrFP6tL8="));
 	}
 
 	public String jasyptEncoding(String value) {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+    SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+    config.setPassword(""); // 사용할 기본 비밀번호 설정
+    config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256"); // 사용할 암호화 알고리즘 설정
+    config.setPoolSize("1"); // 인스턴스 풀 사이즈 설정
+    config.setStringOutputType("base64"); // 인코딩 방식 설정
+    config.setKeyObtentionIterations("1000");
+    config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+    config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+    encryptor.setConfig(config);
+
+		return encryptor.encrypt(value);
+	}
+
+	public String jasyptDecoding(String value) {
 		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
     SimpleStringPBEConfig config = new SimpleStringPBEConfig();
     config.setPassword(""); // 사용할 기본 비밀번호 설정
